40af8527886ce92da8b1d86a5cc77dcc16bdb39f
wenlong lyu <lvwenlong_lambda@qq.com>
2019-03-21 16:37:04 +0800

    Update

--- BNN_Dropout.py
+++ BNN_Dropout.py
@@ -8,7 +8,6 @@ from copy import deepcopy
 class NN_Dropout(nn.Module):
 
     def __init__(self, dim, act, num_hidden, num_layers, dropout_rate, dropout_input = False):
-        torch.set_default_dtype(torch.float64)
         super(NN_Dropout, self).__init__()
         self.dim           = dim
         self.act           = act
@@ -54,6 +53,7 @@ class BNN_Dropout:
         self.num_epochs   = conf.get('num_epochs', 40)
         self.tau          = conf.get('tau', 1)
         self.lscale       = conf.get('lscale', 1e-2)
+        self.print_every  = conf.get('print_every', 100)
         self.nn           = NN_Dropout(dim, self.act, self.num_hidden, self.num_layers, self.dropout_rate)
 
     # TODO: logging
@@ -61,18 +61,20 @@ class BNN_Dropout:
     def train(self, X, y):
         self.x_mean     = X.mean(dim = 0)
         self.x_std      = X.std(dim = 0)
-        self.train_x    = (X - self.x_mean) /self.x_std
-        self.train_y    = y
+        self.y_mean     = y.mean()
+        self.y_std      = y.std()
+        self.train_x    = (X - self.x_mean) / self.x_std
+        self.train_y    = (y - self.y_mean) / self.y_std
         num_train       = self.train_x.shape[0]
-        l2_reg          = self.lscale**2 * (1 - self.dropout_rate) / (2. * num_train * self.tau)
+        self.l2_reg     = self.lscale**2 * (1 - self.dropout_rate) / (2. * num_train * self.tau)
         criterion       = nn.MSELoss()
-        opt             = torch.optim.Adam(self.nn.parameters(), lr = self.lr, weight_decay = l2_reg)
+        opt             = torch.optim.Adam(self.nn.parameters(), lr = self.lr, weight_decay = self.l2_reg)
         dataset         = TensorDataset(self.train_x, self.train_y)
         loader          = DataLoader(dataset, batch_size = self.batch_size, shuffle = True)
-        self.rec_losses = []
         scheduler       = torch.optim.lr_scheduler.StepLR(opt, step_size = int(self.num_epochs / 4), gamma = 0.1)
         # scheduler       = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, min_lr = 1e-6)
         # scheduler       = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max = int(self.num_epochs / 4), eta_min = 1e-8)
+        self.rec_losses = [np.inf for i in range(self.num_epochs)]
         for epoch in range(self.num_epochs):
             scheduler.step()
             for bx, by in loader:
@@ -83,16 +85,34 @@ class BNN_Dropout:
                     loss.backward(retain_graph = True)
                     return loss
                 opt.step(closure)
-            if epoch > 0 and epoch % 10 == 0:
-                true_loss = criterion(self.nn(self.train_x), self.train_y)
+            true_loss     = criterion(self.nn(self.train_x), self.train_y)
+            self.rec_losses[epoch] = true_loss.item()
+            if (epoch + 1) % self.print_every == 0:
                 print("After %d epochs, loss is %g" % (epoch + 1, true_loss))
-                self.rec_losses.append(true_loss)
     
     def predict(self, x):
         self.nn.eval()
         pred = self.nn((x - self.x_mean) / self.x_std)
-        return pred
+        return pred * self.y_std + self.y_mean
 
+    def predict_mv(self, x, n_samples = 20):
+        nns   = [self.sample() for i in range(n_samples)]
+        preds = torch.zeros(x.shape[0], n_samples)
+        for i in range(n_samples):
+            preds[:, i] = nns[i]((x - self.x_mean) / self.x_std).reshape((x.shape[0], ))
+        preds = preds * self.y_std + self.y_mean
+        return preds.mean(dim = 1), preds.var(dim = 1)
+
+    def validate(self, x_test, y_test, n_samples = 20):
+        m, v = self.predict_mv(x_test)
+        m    = m.reshape(y_test.shape)
+        v    = v.reshape(y_test.shape)
+        s    = v.sqrt()
+        mse  = torch.nn.MSELoss()(m.reshape(y_test.shape), y_test)
+        rmse = torch.sqrt(mse).item()
+        nll  = -1 * np.mean([torch.distributions.Normal(m[i], s[i]).log_prob(y_test[i]).item() for i in range(x_test.shape[0])])
+        return rmse, nll
+        
     def sample(self):
         net = deepcopy(self.nn.nn)
         bs  = torch.distributions.Bernoulli(1 - self.dropout_rate)
