962ea7d20cd32cc47bc74a1dba71f80d40fb6b94
wenlong lyu <lvwenlong_lambda@qq.com>
2019-04-06 20:05:03 +0800

    Test BNN

--- BNN.py
+++ BNN.py
@@ -26,24 +26,23 @@ class BNN(ABC):
     def sample_predict(self, nns, input):
         pass
 
-    def validate(self, X, y, num_samples):
-        num_test     = X.shape[0]
-        y            = y.reshape(num_test)
-        post_samples = self.sample(num_samples)
-        nn_samples   = [s[0] for s in post_samples]
-        prec_samples = [s[1] for s in post_samples]
-        preds        = self.sample_predict(nn_samples, X)
-        noise_vars   = 1 / prec_samples
-        py           = preds.mean(dim = 0)
-        pv           = preds.var(dim = 0) + noise_vars
-        rmse         = torch.sqrt(torch.mean((py - y)**2))
-        nll_gaussian = -1 * torch.distributions.Normal(py, pv.sqrt()).log_prob(y).mean()
-        normed       = (y - preds) / noise_vars.sqrt()
-        lls          = torch.logsumexp(-0.5 * normed**2 - 0.5 * torch.log(2 * np.pi * noise_var.unsqueeze(1)), dim = 0) - np.log(num_samples)
+    def validate(self, X, y, num_samples = 20):
+        with torch.no_grad():
+            nn_samples, prec_samples = self.sample(num_samples)
+            num_test     = X.shape[0]
+            y            = y.reshape(num_test)
+            preds        = self.sample_predict(nn_samples, X)
+            noise_vars   = 1 / prec_samples
+            py           = preds.mean(dim = 0)
+            pv           = preds.var(dim = 0) + noise_vars.mean()
+            rmse         = torch.sqrt(torch.mean((py - y)**2))
+            nll_gaussian = -1 * torch.distributions.Normal(py, pv.sqrt()).log_prob(y).mean()
+            normed       = (y - preds) / noise_vars.sqrt().mean()
+            lls          = torch.logsumexp(-0.5 * normed**2 - 0.5 * torch.log(2 * np.pi * noise_vars.unsqueeze(1)), dim = 0) - np.log(num_samples)
         return rmse, nll_gaussian, -1 * lls.mean()
 
-    def predict_mv(self, input, num_samples = 20):
+    def predict_mv(self, input, nn_samples, prec_samples):
         num_test   = input.shape[0]
-        nn_samples = [s[0] for s in self.sample(num_samples)]
         preds      = self.sample_predict(nn_samples, input)
-        return preds.mean(dim = 0), preds.var(dim = 0)
+        noise_vars = 1 / prec_samples
+        return preds.mean(dim = 0), preds.var(dim = 0) + noise_vars.mean()
