08022f38babe3e93df52701b808dfd8ffb04c76b
wenlong lyu <lvwenlong_lambda@qq.com>
2019-05-17 16:37:51 +0800

    Update

--- BO.py
+++ BO.py
@@ -1,51 +1,82 @@
+import sys, os
 import torch
-import torch.nn
+import torch.nn as nn
 import numpy as np
 from BNN_SGDMC import BNN_SGDMC
 from platypus import NSGAII, MOEAD, CMAES, Problem, Real, SPEA2, NSGAIII, Solution, InjectedPopulation, Archive
+import matplotlib.pyplot as plt
 
 class BO:
-    def __init__(f, lb, ub, nobj, ncons, max_eval = 2, num_init = 1, conf = {}):
+    def __init__(self, f, dim, nobj, ncons, max_eval = 2, num_init = 1, act = nn.Tanh(), num_hiddens = [50], conf = {}):
         """
-        f: objective function, with (-1, dim) tensor as input
+        f: objective function, with (-1, dim) tensor as input, with input range [0, 1]
         """
         self.f        = f
-        self.lb       = torch.as_tensor(lb)
-        self.ub       = torch.as_tensor(ub)
-        self.dim      = len(lb)
+        self.dim      = dim
         self.nobj     = nobj
         self.ncons    = ncons
         self.max_eval = max_eval
-        self.bnn      = BNN_SGDMC(self.dim, act = nn.Tanh(), [50, 50], conf)
-        self.X        = torch.randn(num_init, self.dim) * (self.ub - self.lb) + self.lb
+        self.bnn      = BNN_SGDMC(self.dim, act = act, num_hiddens = num_hiddens, conf = conf)
+        self.X        = torch.rand(num_init, self.dim)
         self.y        = self.f(self.X)
         assert(self.y.dim() == 2)
         assert(self.y.shape[1] == self.nobj + self.ncons)
 
+    def see(self):
+        assert(self.dim == 1)
+        assert(self.nobj + self.ncons == 1)
+        xs = torch.linspace(0.,1.,100).view(-1,1)
+        with torch.no_grad():
+            pred = self.bnn.sample_predict(self.bnn.nns, (xs - self.x_mean) / self.x_std)
+            pred = pred * self.y_std + self.y_mean
+        plt.plot(xs.numpy(), pred.squeeze().t().numpy(), 'g', alpha = 0.1)
+
+        plt.plot(self.X.numpy(), self.y.numpy(), 'k+')
+        plt.show()
+
     def normalize(self):
-        self.x_mean = self.X.mean(dim = 1)
-        self.x_std  = self.X.std(dim = 1)
-        self.y_mean = self.y.mean(dim = 1)
-        self.y_std  = self.y.std(dim = 1)
-        X           = (self.X - x_mean) / self.x_std
-        y           = (self.y - y_mean) / self.y_std
+        self.x_mean = torch.zeros(self.dim)
+        self.x_std  = torch.ones(self.dim)
+        self.y_mean = torch.zeros(self.nobj + self.ncons)
+        self.y_std  = torch.ones(self.nobj + self.ncons)
+        # if self.X.shape[0] == 1:
+        #     self.x_mean = torch.zeros(self.dim)
+        #     self.x_std  = torch.ones(self.dim)
+        #     self.y_mean = torch.zeros(self.nobj + self.ncons)
+        #     self.y_std  = torch.ones(self.nobj + self.ncons)
+        # else:
+        #     self.x_mean = self.X.mean(dim = 0)
+        #     self.x_std  = self.X.std(dim  = 0)
+        #     self.y_mean = self.y.mean(dim = 0)
+        #     self.y_std  = self.y.std(dim  = 0)
+        X           = (self.X - self.x_mean) / self.x_std
+        y           = (self.y - self.y_mean) / self.y_std
+        self.n_lb   = (torch.zeros(self.dim) - self.x_mean) / self.x_std # normalized lower bound
+        self.n_ub   = (torch.ones(self.dim)  - self.x_mean) / self.x_std
         return X, y
 
     def train(self):
-        X, y = self.normalize(self.X, self.y)
+        X, y = self.normalize()
         self.bnn.train(X, y)
+        for nn in self.bnn.nns:
+            for p in nn.parameters():
+                p.requires_grad = False
     
+
     def nn_opt(self, nn):
         with torch.no_grad():
             def obj_cons(x):
                 tx  = torch.tensor(x)
-                out = nn(tx)
+                out = nn(tx) 
                 return out[:self.nobj].numpy().tolist(), out[self.nobj:].numpy().tolist()
             def obj_ucons(x):
                 tx  = torch.tensor(x)
                 return nn(tx).numpy().tolist()
 
-            arch          = Archive()
+            lb = (torch.zeros(self.dim) - self.x_mean) / self.x_std
+            ub = (torch.ones(self.dim)  - self.x_mean) / self.x_std
+
+            arch = Archive()
             if self.ncons == 0:
                 prob          = Problem(self.dim, self.nobj)
                 prob.function = obj_ucons
@@ -53,26 +84,32 @@ class BO:
                 prob                = Problem(self.dim, self.nobj, self.ncons)
                 prob.function       = obj_cons
                 prob.constraints[:] = "<=0"
-            prob.types[:] = [Real(self.lb[i], self.ub[i]) for i in range(self.dim)]
-            algo          = CMAES(prob, population = 100, archive = arch)
-            def cb(a):
-                print(a.nfe, len(a.archive), flush=True)
-            algo.run(100, callback = cb)
+            prob.types[:] = [Real(lb[i], ub[i]) for i in range(self.dim)]
+            self.algo     = NSGAII(prob, population = 50, archive = arch)
+            self.algo.run(5000)
 
-            optimized = algorithm.population
-            rand_idx  = np.random.randint(len(optimized))
-            suggested = torch.tensor(optimized[rand_idx].variables)
-            return suggested
+            optimized   = self.algo.population
+            rand_idx    = np.random.randint(len(optimized))
+            suggested_x = torch.tensor(optimized[rand_idx].variables) 
+            suggested_y = nn(suggested_x)
+            suggested_x = suggested_x * self.x_std + self.x_mean
+            suggested_y = suggested_y * self.y_std + self.y_mean
+            return suggested_x.view(-1, self.dim), suggested_y.view(-1, self.nobj + self.ncons)
     
     def bo_iter(self, num_samples = 1):
-        assert(num_samples <= self.bnn.num_samples)
         self.train()
-        nn_idxs   = torch.randperm(self.bnn.num_samples)[:num_sample]
-        nns       = self.bnn.nns[nn_idxs]
-        suggested = torch.tensor([self.nn_opt(nn) for nn in nns])
+        self.see()
+        assert(num_samples <= len(self.bnn.nns))
+        nn_idxs   = torch.randperm(len(self.bnn.nns))[:num_samples]
+        suggested = torch.zeros(num_samples, self.dim)
+        for i in range(num_samples):
+            sx, sy       = self.nn_opt(self.bnn.nns[i])
+            suggested[i] = sx
         evaluated = self.f(suggested)
         self.X    = torch.cat((self.X, suggested))
         self.y    = torch.cat((self.y, evaluated))
+        print(suggested)
+        print(evaluated)
     
     def OSFTA(self):
         """
--- Benchmarks.py
+++ Benchmarks.py
@@ -3,6 +3,8 @@ import numpy as np
 import hpolib
 
 ## Single-objective benchmarks from hpolib
+def quad(x):
+    return torch.sum((10*(x - 0.5))**2, dim = 1).view(-1, 1)
 
 ## Constrained optimization problems from CEC
 
